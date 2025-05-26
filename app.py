57aea8c0bb8b9950a13738200315cfe7a889b195
Tova501 <TA0556736501@GMAIL.COM>
2025-05-26 20:19:32 +0300

    Change port settings

--- app.py
+++ app.py
@@ -104,5 +104,6 @@ def ai_analyzer():
     return jsonify({"response": answer})
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
